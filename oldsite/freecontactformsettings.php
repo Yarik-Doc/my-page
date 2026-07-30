92f12e04fdc7a2f0824ea083b95cb9e49746e1f7
Yarik-Doc <83291876+Yarik-Doc@users.noreply.github.com>
2026-07-30 10:57:02 +0200

    Add files via upload
    new

---
+++ oldsite/freecontactformsettings.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+$email_to = "yaroslav.hry@gmail.com"; // your email address goes in place of you@yourdomain.com. MAKE SURE THAT YOU LEAVE THE QUOTES ("") IN PLACE
+
+$email_subject = "Contact Form Message"; // email subject line will show up on the received email.
+
+$thankyou = "thankyou.html"; // thank you page shows once the form has been sent to let users know that the form is sent then redirects the user back to your homepage after 8/9 seconds
+
+
+
+// if you update the Anti Spam question on the form -
+
+// you need to update the questions answer below
+
+$antispam_answer = "25";
+
+
+
+?>
